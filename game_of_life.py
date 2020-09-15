5673b9b34d025627c11c41e0d78680ef099429b2
Álvaro del Hoyo <32258439+alvarodh@users.noreply.github.com>
2020-09-15 18:01:33 +0200

    Update game_of_life.py
    añadidos botones para el idioma

--- game_of_life.py
+++ game_of_life.py
@@ -1,14 +1,16 @@
-import pygame, time, random, sys
+import pygame, time, random, sys, settings
+from button_class import *
 import numpy as np
 
 class Game:
 
-    def __init__(self, width, height, nxC, nyC):
+    def __init__(self, width, text_width, height, nxC, nyC, language):
         pygame.init()
         # Estado de las celdas:
         #   - 1: celula viva
         #   - 0: celula muerta
         self.width = width
+        self.text_width = text_width
         self.height = height
         self.nxC = nxC
         self.nyC = nyC
@@ -19,6 +21,9 @@ class Game:
         self.pause = True
         self.cell_color = [[128, 128, 128], [255, 255, 255]]
         self.font = pygame.font.Font('KidGames.ttf',64)
+        self.language = language
+        self.playing_buttons = []
+        self.load_buttons()
 
     def event_handler(self):
         for event in pygame.event.get():
@@ -26,6 +31,7 @@ class Game:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit('user quit')
+                
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     self.pause = not self.pause
@@ -107,6 +113,11 @@ class Game:
                 celX, celY = int(np.floor(posX / self.dimCW)), int(np.floor(posY / self.dimCH))
                 if celX < self.nxC and celY < self.nyC:
                     self.gameState[celX, celY] = not pygame.mouse.get_pressed()[2]
+                else:
+                    for button in self.playing_buttons:
+                        button.update(pygame.mouse.get_pos())
+                        if button.highLighted:
+                            button.click()
 
     def update(self, x, y):
         # Calculamos los vecinos cercanos
@@ -126,10 +137,13 @@ class Game:
             self.newGameState[x, y] = 1
         elif self.gameState[x, y] == 1 and (n_neigh < 2 or n_neigh > 3):
             self.newGameState[x, y] = 0
+        
+        
 
     def draw(self, screen):
 
         self.draw_pause(screen)
+        self.write_instructions(screen)
         self.newGameState = np.copy(self.gameState)
 
         for y in range(self.nxC):
@@ -151,15 +165,18 @@ class Game:
                 pygame.draw.polygon(screen, color, poly, border)
         # Actualizamos el estado
         self.gameState = np.copy(self.newGameState)
+        
+        for button in self.playing_buttons:
+            button.draw(screen)
 
     def draw_pause(self, screen):
 
         if self.pause:
             color_pause = (255,0,0)
-            text_pause = 'PAUSE'
+            text_pause = settings.pause_text[self.language][0]
         else:
             color_pause = (0,255,0)
-            text_pause = 'RUNNING'
+            text_pause = settings.pause_text[self.language][1]
 
         text = self.font.render(text_pause, True, color_pause)
         w = self.width + (500 - text.get_width()) / 2
@@ -172,13 +189,7 @@ class Game:
         x = random.randint(0, self.nxC - 4)
         y = random.randint(0, self.nyC - 8)
 
-        self.gameState[x:x+7,y:y+3] = [[0,0,1],
-                                       [1,0,1],
-                                       [0,0,0],
-                                       [0,1,0],
-                                       [0,0,1],
-                                       [0,0,1],
-                                       [0,0,1]]
+        self.gameState[x:x+7,y:y+3] = settings.acorn
 
     # Automata palo 1x3
     def draw_stick(self):
@@ -186,7 +197,7 @@ class Game:
         x = random.randint(0, self.nxC - 1)
         y = random.randint(0, self.nyC - 4)
 
-        self.gameState[x,y:y+3] = [1,1,1]
+        self.gameState[x,y:y+3] = settings.stick
 
     # Automata movil 3x3
     def draw_movil3x3(self):
@@ -194,9 +205,7 @@ class Game:
         x = random.randint(0, self.nxC - 4)
         y = random.randint(0, self.nyC - 4)
 
-        self.gameState[x:x+3,y:y+3] = [[0,1,0],
-                                       [0,0,1],
-                                       [1,1,1]]
+        self.gameState[x:x+3,y:y+3] = settings.movil3x3
 
     # Automata movil 7x4
     def draw_movil7x4(self):
@@ -204,13 +213,7 @@ class Game:
         x = random.randint(0, self.nxC - 8)
         y = random.randint(0, self.nyC - 5)
 
-        self.gameState[x:x+7,y:y+4] = [[0,1,1,0],
-                                       [1,1,1,0],
-                                       [1,1,1,0],
-                                       [1,1,1,0],
-                                       [1,1,0,1],
-                                       [0,1,1,1],
-                                       [0,0,1,0]]
+        self.gameState[x:x+7,y:y+4] = settings.movil7x4
 
     # Oscilador 13x13
     def draw_oscilator13x13(self):
@@ -218,19 +221,7 @@ class Game:
         x = random.randint(0, self.nxC - 14)
         y = random.randint(0, self.nyC - 14)
 
-        self.gameState[x:x+13,y:y+13] = [[0,0,1,1,0,0,0,0,0,1,1,0,0],
-                                         [0,0,0,1,1,0,0,0,1,1,0,0,0],
-                                         [1,0,0,1,0,1,0,1,0,1,0,0,1],
-                                         [1,1,1,0,1,1,0,1,1,0,1,1,1],
-                                         [0,1,0,1,0,1,0,1,0,1,0,1,0],
-                                         [0,0,1,1,1,0,0,0,1,1,1,0,0],
-                                         [0,0,0,0,0,0,0,0,0,0,0,0,0],
-                                         [0,0,1,1,1,0,0,0,1,1,1,0,0],
-                                         [0,1,0,1,0,1,0,1,0,1,0,1,0],
-                                         [1,1,1,0,1,1,0,1,1,0,1,1,1],
-                                         [1,0,0,1,0,1,0,1,0,1,0,0,1],
-                                         [0,0,0,1,1,0,0,0,1,1,0,0,0],
-                                         [0,0,1,1,0,0,0,0,0,1,1,0,0]]
+        self.gameState[x:x+13,y:y+13] = settings.oscilator13x13
 
     # Oscilador 12x12
     def draw_oscilator12x12(self):
@@ -238,18 +229,7 @@ class Game:
         x = random.randint(0, self.nxC - 13)
         y = random.randint(0, self.nyC - 13)
 
-        self.gameState[x:x+12,y:y+12] = [[0,0,0,0,0,0,1,1,0,0,0,0],
-                                         [0,0,0,0,0,0,1,1,0,0,0,0],
-                                         [0,0,0,0,0,0,0,0,0,0,0,0],
-                                         [0,0,0,0,1,1,1,1,0,0,0,0],
-                                         [1,1,0,1,0,0,0,0,1,0,0,0],
-                                         [1,1,0,1,0,0,1,0,1,0,0,0],
-                                         [0,0,0,1,0,0,0,1,1,0,1,1],
-                                         [0,0,0,1,0,1,0,0,1,0,1,1],
-                                         [0,0,0,0,1,1,1,1,0,0,0,0],
-                                         [0,0,0,0,0,0,0,0,0,0,0,0],
-                                         [0,0,0,0,1,1,0,0,0,0,0,0],
-                                         [0,0,0,0,1,1,0,0,0,0,0,0]]
+        self.gameState[x:x+12,y:y+12] = settings.oscilator12x12
 
     # Oscilador 10x3
     def draw_oscilator10x3(self):
@@ -257,16 +237,7 @@ class Game:
         x = random.randint(0, self.nxC - 11)
         y = random.randint(0, self.nyC - 4)
 
-        self.gameState[x:x+10,y:y+3] = [[0,1,0],
-                                        [0,1,0],
-                                        [1,0,1],
-                                        [0,1,0],
-                                        [0,1,0],
-                                        [0,1,0],
-                                        [0,1,0],
-                                        [1,0,1],
-                                        [0,1,0],
-                                        [0,1,0]]
+        self.gameState[x:x+10,y:y+3] = settings.oscilator10x3
 
     # Oscilador 9x9
     def draw_oscilator9x9(self):
@@ -274,15 +245,7 @@ class Game:
         x = random.randint(0, self.nxC - 10)
         y = random.randint(0, self.nyC - 10)
 
-        self.gameState[x:x+9,y:y+9] = [[1,1,0,1,1,1,1,1,1],
-                                       [1,1,0,1,1,1,1,1,1],
-                                       [1,1,0,0,0,0,0,0,0],
-                                       [1,1,0,0,0,0,0,1,1],
-                                       [1,1,0,0,0,0,0,1,1],
-                                       [1,1,0,0,0,0,0,1,1],
-                                       [0,0,0,0,0,0,0,1,1],
-                                       [1,1,1,1,1,1,0,1,1],
-                                       [1,1,1,1,1,1,0,1,1]]
+        self.gameState[x:x+9,y:y+9] = settings.oscilator9x9
 
     # Oscilador 6x6
     def draw_oscilator6x6(self):
@@ -290,12 +253,7 @@ class Game:
         x = random.randint(0, self.nxC - 7)
         y = random.randint(0, self.nyC - 7)
 
-        self.gameState[x:x+6,y:y+6] = [[0,1,0,0,1,0],
-                                       [1,0,1,1,0,1],
-                                       [0,1,0,0,1,0],
-                                       [0,1,0,0,1,0],
-                                       [1,0,1,1,0,1],
-                                       [0,1,0,0,1,0]]
+        self.gameState[x:x+6,y:y+6] = settings.oscilator6x6
 
     # Oscilador 4x4
     def draw_oscilator4x4(self):
@@ -303,10 +261,7 @@ class Game:
         x = random.randint(0, self.nxC - 5)
         y = random.randint(0, self.nyC - 5)
 
-        self.gameState[x:x+4,y:y+4] = [[0,1,0,0],
-                                       [0,1,0,1],
-                                       [1,0,1,0],
-                                       [0,0,1,0]]
+        self.gameState[x:x+4,y:y+4] = settings.oscilator4x4
 
     # Pistola 36x9
     def draw_gun36x9(self):
@@ -314,15 +269,7 @@ class Game:
         x = random.randint(0, self.nxC - 10)
         y = random.randint(0, self.nyC - 37)
 
-        self.gameState[x:x+9,y:y+36] = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
-                                        [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1],
-                                        [1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                                        [1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+        self.gameState[x:x+9,y:y+36] = settings.gun36x9
 
     # Pistola 9x36
     def draw_gun9x36(self):
@@ -330,85 +277,35 @@ class Game:
         x = random.randint(0, self.nxC - 37)
         y = random.randint(0, self.nyC - 10)
 
-        self.gameState[x:x+36,y:y+9] = [[0,0,0,0,1,1,0,0,0],
-                                        [0,0,0,0,1,1,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,1,1,1,0,0],
-                                        [0,0,0,1,0,0,0,1,0],
-                                        [0,0,1,0,0,0,0,0,1],
-                                        [0,0,1,0,0,0,0,0,1],
-                                        [0,0,0,0,0,1,0,0,0],
-                                        [0,0,0,1,0,0,0,1,0],
-                                        [0,0,0,0,1,1,1,0,0],
-                                        [0,0,0,0,0,1,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,1,1,1,0,0,0,0],
-                                        [0,0,1,1,1,0,0,0,0],
-                                        [0,1,0,0,0,1,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [1,1,0,0,0,1,1,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,0,0,0,0,0,0,0],
-                                        [0,0,1,1,0,0,0,0,0],
-                                        [0,0,1,1,0,0,0,0,0]]
+        self.gameState[x:x+36,y:y+9] = settings.gun9x36
 
 
-instructions = ['',
-                '------------------ USAGE ------------------',
-                'CLICK A CELL WITH MOUSE BUTTON:',
-                '    - RIGHT: FOR PUT IT ALIVE',
-                '    - LEFT: FOR PUT IT DEATH',
-                '-------------------------------------------',
-                'PRESS SPACEBAR FOR PAUSE',
-                'IT\'S EASIER TO DRAW WHEN THE GAME IT\'S',
-                'PAUSED',
-                '-------------------------------------------',
-                'PRESS LETTERS FOR DRAW PATRONS:',
-                '    - A: DRAW ACORN PATRON',
-                '    - U: DRAW A 13x13 OSCILATOR',
-                '    - K: DRAW A 12x12 OSCILATOR',
-                '    - Q: DRAW A 10X3 OSCILATOR',
-                '    - O: DRAW A 9X9 OSCILATOR',
-                '    - S: DRAW A 6X6 OSCILATOR',
-                '    - W: DRAW A 4X4 OSCILATOR',
-                '    - V: DRAW A 36X9 PLANES GUN',
-                '    - H: DRAW A 9X36 PLANES GUN',
-                '    - X: DRAW A 3X3 MOVIL AUTOMATON',
-                '    - N: DRAW A 7X4 MOVIL AUTOMATON',
-                '    - P: DRAW A 1X3 STICK AUTOMATON',
-                '    - D: DELETE ALL THE SCREEN',
-                '-------------------------------------------',
-                'SELECT THE CELL COLOR:',
-                '    - PRESS R: THE CELL WILL BE RED',
-                '    - PRESS G: THE CELL WILL BE GREEN',
-                '    - PRESS B: THE CELL WILL BE BLUE',
-                '    - PRESS C: THE CELL WILL BE CYAN',
-                '    - PRESS Y: THE CELL WILL BE YELLOW',
-                '    - PRESS M: THE CELL WILL BE MAGENTA',
-                '    - PRESS Z: THE CELL WILL A RANDOM COLOR',
-                '-------------------------------------------',
-                'PRESS ESC FOR CLOSE THE WINDOW']
+    def write_instructions(self, screen):
+        font = pygame.font.Font('CutiveMono-Regular.ttf',16)
+        instruct = settings.instructions[self.language]
+        text_height = (self.height - 150) / len(instruct)
+        for i in range(len(instruct)):
+            screen.blit(font.render(instruct[i], True, (255,255,255)),
+                        (self.width + 50, 50 + (text_height * (i+1))))
 
-def write_instructions(screen, font, width, height):
-    text_height = (height - 50) / len(instructions)
-    for i in range(len(instructions)):
-        screen.blit(font.render(instructions[i], True, (255,255,255)), (width, 50 + (text_height * (i+1))))
+    def change_language(self, language):
+        self.language = language
 
+    def load_buttons(self):
+    
+        self.playing_buttons.append(Button(self.width + 50,
+                                           self.height - 50, 100, 40,
+                                           function=self.change_language,
+                                           params='english',
+                                           colour=(27,142,207),
+                                           text='ENGLISH'))
+        self.playing_buttons.append(Button(self.width + self.text_width - 150,
+                                           self.height - 50, 100, 40,
+                                           function=self.change_language,
+                                           params='spanish',
+                                           colour=(27,142,207),
+                                           text='SPANISH',
+                                           highLighted=True))
 
 if __name__ == '__main__':
     # Crear ventana de tamaño definido
@@ -419,18 +316,15 @@ if __name__ == '__main__':
     # Color de fondo y pintar el fondo
     bg = 30, 30, 30
     screen.fill(bg)
-    game = Game(width - text_width, height, 50, 50)
-    # Fuentes
-    #font = pygame.font.Font('Anton-Regular.ttf',16)
-    font = pygame.font.Font('CutiveMono-Regular.ttf',16)
-    #font = pygame.font.Font('KidGames.ttf',16)
-
+    # Crear el juego
+    game = Game(width - text_width, text_width, height, 50, 50, 'spanish')
+    
+    # Jugar
     while True:
 
         try:
 
             screen.fill(bg)
-            write_instructions(screen, font, width - text_width + 50, height - 100)
             time.sleep(0.1)
             game.event_handler()
             game.draw(screen)
